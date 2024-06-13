afd2fba612d9a57cac4a99426b78cb5023f2944f
IkennaIsineyi <ikenna.isineyi@brilloconnetz.com>
2024-06-13 01:09:15 +0100

    path

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import ITdataSlice from './data/itDataSlice';
+import itDataSlice from './data/itDataSlice';
 import designDataSlice from './data/designDataSlice';
 import itDevDataSlice from './data/itDevDataSlice';
 import designDataCardSlice from './data/designDataCardSlice';
@@ -12,7 +12,7 @@ import footerDataSlice from './data/footerDataSlice';
 
 const store = configureStore({
   reducer: {
-    itData: ITdataSlice,
+    itData: itDataSlice,
     designData: designDataSlice,
     itDevDataCard: itDevDataSlice,
     designDataCard: designDataCardSlice,
